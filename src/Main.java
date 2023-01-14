ec367dd7021449a5edc52b784a9f8bffc8e92b17
wish9 <wjwee9@gmail.com>
2023-01-14 17:06:06 +0900

    데일리코딩 1번

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] arr = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
+        TransformFirstAndLast transform = new TransformFirstAndLast();
+
+
+        HashMap<String, String> output = transform.transformFirstAndLast(arr);
+        System.out.println(output);
     }
 }
---
+++ src/TransformFirstAndLast.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class TransformFirstAndLast {
+
+    //String[] arr = new String[]{'Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'}; // input예시
+    public HashMap<String, String> transformFirstAndLast(String[] arr) {
+
+        if(arr.length==0){
+            return null;
+        }else{
+            HashMap<String,String> result = new HashMap<>();
+            result.put(arr[0],arr[arr.length-1]);
+
+            return result;
+        }
+    }
+}
