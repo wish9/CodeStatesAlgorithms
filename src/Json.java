7c57981810d7d2b2d1eecadfc0a1874e5c0dca17
wish9 <wjwee9@gmail.com>
2023-01-15 20:33:17 +0900

    재귀연습(stringfy메소드 만들기)

---
+++ src/Json.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Json {
+    public String stringify(Object data) {
+
+        //입력된 값이 문자열일 경우
+        if(data instanceof String){
+            String text = "\"";
+            return text+data+text;
+        }
+
+        //입력된 값이 Integer일 경우
+        else if(data instanceof Integer){
+            String changed =  Integer.toString((Integer) data);
+            return changed;
+        }
+
+        //입력된 값이 Boolean일 경우
+        else if(data instanceof Boolean){
+            String changed =  String.valueOf(data);
+            return changed;
+        }
+
+        //입력된 값이 Object[]일 경우
+
+        else if(data instanceof Object[]){
+            if(((Object[]) data).length == 0) return  "[]";
+            String str = "[";
+
+            Iterator<?> it = Arrays.asList((Object[])data).iterator();
+            while (it.hasNext()) {
+                str += stringify(it.next()) + ",";
+            }
+            str = str.substring(0,str.length()-1);
+            str += "]";   // Iterator 2차 실패... 무한루프..
+            return str;
+        }
+
+        //입력된 값이 HashMap일 경우
+        else if(data instanceof HashMap){
+
+            if(((HashMap) data).isEmpty()){
+                return "{}";
+            }
+            String res = "{";
+            for (Map.Entry<?, ?> o : ((HashMap<?, ?>) data).entrySet()) {
+                res += stringify(o.getKey())+ ":" + stringify(o.getValue())+",";
+            }
+            res = res.substring(0,res.length()-1);
+            res += "}";
+
+            return res;
+        }
+
+        //지정되지 않은 타입의 경우에는 "null"을 리턴합니다.
+        return "null";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        //DailyCoding 코플릿 1번
 //        String[] arr = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
 //        TransformFirstAndLast transform = new TransformFirstAndLast();
 //
@@ -9,10 +10,33 @@ public class Main {
 //        HashMap<String, String> output = transform.transformFirstAndLast(arr);
 //        System.out.println(output);
 
+        //DailyCoding 코플릿 2번
+//        TransformFirstAndLast transform = new TransformFirstAndLast();
+//
+//        System.out.println(transform.computeWhenDouble(7));
+//        System.out.println(transform.computeWhenDouble(2.3));
 
-        TransformFirstAndLast transform = new TransformFirstAndLast();
 
-        System.out.println(transform.computeWhenDouble(7));
-        System.out.println(transform.computeWhenDouble(2.3));
+        //stringfy 메서드 (자바타입 -> JSON타입) 수동으로 만들기
+        Json json = new Json();
+
+        System.out.println(json.stringify(77));
+        System.out.println(json.stringify("stringfy"));
+        System.out.println(json.stringify(true));
+        System.out.println(json.stringify(new String[]{"a", "b", "c", "d", "e"}));
+        System.out.println(json.stringify(new Object[]{7, new Object[]{new Object[]{}, 1, 2}}));
+        System.out.println(json.stringify(new Object[]{new Object[]{new Object[]{}}}));
+
+        HashMap<Object, Object> hashMap = new HashMap<>(){{
+            put("a","apple");
+            put("7",77);
+            put("true", true);
+            put("false", false);
+            put("null", null);
+        }};
+
+        System.out.println(json.stringify(hashMap));
+
+
     }
 }
