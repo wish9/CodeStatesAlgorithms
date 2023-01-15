200f55a20b5f55328e16d96ad0936d2f1c90aaf6
wish9 <wjwee9@gmail.com>
2023-01-15 19:30:45 +0900

    데일리코딩 2번(연이율->원금 두배 시간)

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arr = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
+//        String[] arr = new String[]{"Queen", "Elizabeth", "Of Hearts", "Beyonce"};
+//        TransformFirstAndLast transform = new TransformFirstAndLast();
+//
+//
+//        HashMap<String, String> output = transform.transformFirstAndLast(arr);
+//        System.out.println(output);
+
+
         TransformFirstAndLast transform = new TransformFirstAndLast();
 
-
-        HashMap<String, String> output = transform.transformFirstAndLast(arr);
-        System.out.println(output);
+        System.out.println(transform.computeWhenDouble(7));
+        System.out.println(transform.computeWhenDouble(2.3));
     }
 }
--- src/TransformFirstAndLast.java
+++ src/TransformFirstAndLast.java
@@ -14,4 +14,19 @@ public class TransformFirstAndLast {
             return result;
         }
     }
+
+
+
+    public int computeWhenDouble(double interestRate) { // 연이율을 입력받아 원금이 2배 이상이 되는 시간(년)을 리턴
+        double principal = 100;
+        int result = 0;
+        for(int i=1; i<100; i++){
+            principal = principal*(100+interestRate)/100;
+            if(principal>=200) {
+                result=i;
+                break;
+            }
+        }
+        return result;
+    }
 }
