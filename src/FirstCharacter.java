1604ae76b32765deab7322c8d96a75876c61452c
wish9 <wjwee9@gmail.com>
2023-01-17 09:59:25 +0900

    DailyCoding - 문자열 앞글자 분리

---
+++ src/FirstCharacter.java
@@ -0,0 +1,12 @@
+public class FirstCharacter {
+    public String firstCharacter(String str) { //문자열의 단어별(공백으로 구분) 첫문자 출력하기
+        String result = "";
+        String[] strA = str.split(" ");
+        if(str.length()==0) return "";
+
+        for(int i =0; i<strA.length; i++) {
+            result += strA[i].charAt(0);
+        }
+        return result;
+    }
+}
